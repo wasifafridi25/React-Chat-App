fc4d6fbc692acedfad049d8552c1d8f00201fbf6
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-10-06 03:08:10 -0700

    Finished the sidebar design

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Cam from '../Assets/cam.png'
+import Add from '../Assets/add.png'
+import More from '../Assets/more.png'
 
 export default function Chat() {
   return (
     <div className='chat'>
-      Chat
+      <div className="chat__info">
+        <span>Wasif</span>
+        <div className="chat__icons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
