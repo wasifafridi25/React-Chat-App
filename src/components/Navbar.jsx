90dcf0ae16ba3f351db6ff4a675ddf3016ca3d61
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-10-13 19:17:30 -0700

    Adding the search feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import { signOut } from 'firebase/auth'
-import React from 'react'
+import React, { useContext } from 'react'
 import { auth } from '../Firebase'
+import { AuthContext } from '../context/AuthContext'
 
 export default function Navbar() {
-  
+  const {currentUser} = useContext(AuthContext)
 
   return (
     <div className='navbar'>
       <span className="logo navbar__logo">BSW Chat</span>
       <div className="navbar__user">
-        <img src="https://images.pexels.com/photos/1816654/pexels-photo-1816654.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" className="navbar__user--img" />
-        <span className="navbar__user--name">Wasif</span>
+        <img src={currentUser.photoURL} alt="" className="navbar__user--img" />
+        <span className="navbar__user--name">{currentUser.displayName}</span>
         <button className="navbar__user--btn" onClick={() => signOut(auth)}>Logout</button>
       </div>
     </div>
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import { collection, query, where } from "firebase/firestore";
+import { db } from "../Firebase.js";
 
 export default function Searchbar() {
+  const search = () => {
+    const [username, setUsername] = useState("");
+    const [user, setUser] = useState(null);
+    const [err, setErr] = useState(false);
+
+    const handleSearch = async () => {
+      const q = query(
+        collection(db, "users"),
+        where("displayName", "==", username)
+      );
+
+      try {
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          setUser(doc.data());
+          console.log(doc.id, " => ", doc.data());
+        });
+      } catch (err) {
+        setErr(true);
+      }
+    };
+
+    const handleKey = (e) => {
+      e.code === "Enter" && handleSearch();
+    };
+  };
+  
   return (
-    <div className='searchbar'>
-      <input type="text" placeholder='Find a user' className='searchbar__input'/>
-      <div className="user__chat">
-        <img src="https://images.pexels.com/photos/1816654/pexels-photo-1816654.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+    <div className="searchbar">
+      <input
+        type="text"
+        placeholder="Find a user"
+        className="searchbar__input"
+        onKeyDown={handleKey}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      {user && <div className="user__chat">
+        <img
+          src={user.photoURL}
+          alt=""
+        />
         <div className="user__chat--info">
-            <span className='searchbar__span'>Wasif</span>
+          <span className="searchbar__span">{user.displayName}</span>
         </div>
-      </div>
+      </div>}
     </div>
-  )
+  );
 }
