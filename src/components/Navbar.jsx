e290b5fceff24d1639e6d41ae1d8d7e7b77d5266
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-10-22 21:06:25 -0700

    Added skeleton loading state to the navbar profile image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,39 @@
-import { signOut } from 'firebase/auth'
-import React, { useContext } from 'react'
-import { auth } from '../Firebase'
-import { AuthContext } from '../context/AuthContext'
-import Img from "../Assets/default.jpg"
+import { signOut } from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
+import { auth } from "../Firebase";
+import { AuthContext } from "../context/AuthContext";
 
 export default function Navbar() {
-  const {currentUser} = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (currentUser && currentUser.photoURL) {
+        setLoading(false);
+      }
+    }, 1000);
+  }, [currentUser]);
 
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <span className="logo navbar__logo">BSW Chat</span>
       <div className="navbar__user">
-        {currentUser && currentUser.photoURL ? <img src={currentUser.photoURL} alt="" className="navbar__user--img" /> : <img src={Img} alt="" className="navbar__user--img" />}
+        {loading ? (
+          <div className="loading__friend--img navbar__loading--img"></div>
+        ) : (
+          <img
+            src={currentUser.photoURL}
+            alt=""
+            className="navbar__user--img"
+          />
+        )}
 
         <span className="navbar__user--name">{currentUser.displayName}</span>
-        <button className="navbar__user--btn" onClick={() => signOut(auth)}>Logout</button>
+        <button className="navbar__user--btn" onClick={() => signOut(auth)}>
+          Logout
+        </button>
       </div>
     </div>
-  )
+  );
 }
